e7d80495356eab70fab9a52a457c9a946716bc0f
Abhay Kumar <abhay@opensynapse.net>
2009-08-12 19:06:47 -0700

    add macros for those of us who don't have it

--- ext/typhoeus/native.h
+++ ext/typhoeus/native.h
@@ -2,6 +2,15 @@
 #define TYPHOEUS_NATIVE
 
 #include <ruby.h>
+
+#ifndef RSTRING_PTR
+#define RSTRING_PTR(s) (RSTRING(s)->ptr)
+#endif
+
+#ifndef RSTRING_LEN
+#define RSTRING_LEN(s) (RSTRING(s)->len)
+#endif
+
 #include <curl/curl.h>
 #include <curl/easy.h>
 #include <curl/multi.h>
