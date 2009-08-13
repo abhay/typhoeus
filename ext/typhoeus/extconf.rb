c20441c4b8beaf485860cca2f92e538733d425a8
Abhay Kumar <abhay@opensynapse.net>
2009-08-12 19:07:11 -0700

    extconf that works for more systems

--- ext/typhoeus/extconf.rb
+++ ext/typhoeus/extconf.rb
@@ -2,56 +2,22 @@ ENV["ARCHFLAGS"] = "-arch #{`uname -p` =~ /powerpc/ ? 'ppc' : 'i386'}"
 
 require 'mkmf'
 
-ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
-LIBDIR = Config::CONFIG['libdir']
-INCLUDEDIR = Config::CONFIG['includedir']
+dir_config('curl')
 
-  use_macports = !(defined?(RUBY_ENGINE) && RUBY_ENGINE != 'ruby')
-
-$CFLAGS << " #{ENV["CFLAGS"]}"
-if Config::CONFIG['target_os'] == 'mingw32'
-  $CFLAGS << " -DXP_WIN -DXP_WIN32"
-else
-  $CFLAGS << " -g -DXP_UNIX"
+if find_executable('curl-config')
+  $CFLAGS << " #{`curl-config --cflags`.strip}"
+  $LIBS << " #{`curl-config --libs`.strip}"
+elsif !have_library('curl') or !have_header('curl/curl.h')
+  fail <<-EOM
+Can't find libcurl or curl/curl.h
+ 
+Try passing --with-curl-dir or --with-curl-lib and --with-curl-include
+options to extconf.
+EOM
 end
 
-$LIBPATH << "/opt/local/lib" if use_macports
-
-$CFLAGS << " -O3 -Wall -Wcast-qual -Wwrite-strings -Wconversion -Wmissing-noreturn -Winline"
-
-if Config::CONFIG['target_os'] == 'mingw32'
-  header = File.join(ROOT, 'cross', 'curl-7.19.4.win32', 'include')
-  unless find_header('curl/curl.h', header)
-    abort "need libcurl"
-  end
-else
-  HEADER_DIRS = [
-    File.join(INCLUDEDIR, "curl"),
-    INCLUDEDIR,
-    '/usr/include/curl',
-    '/usr/local/include/curl'
-  ]
-
-  [
-    '/opt/local/include/curl',
-    '/opt/local/include',
-  ].each { |x| HEADER_DIRS.unshift(x) } if use_macports
-
-  unless find_header('curl/curl.h', *HEADER_DIRS)
-    abort "need libcurl"
-  end
-end
-
-if Config::CONFIG['target_os'] == 'mingw32'
-  find_library('curl', 'curl_easy_init',
-               File.join(ROOT, 'cross', 'curl-7.19.4.win32', 'bin'))
-else
-  find_library('curl', 'curl_easy_init',
-               LIBDIR,
-               '/opt/local/lib',
-               '/usr/local/lib',
-               '/usr/lib'
-    )
+if try_compile('int main() { return 0; }','-Wall')
+  $CFLAGS << ' -O3 -Wall -Wcast-qual -Wwrite-strings -Wconversion -Wmissing-noreturn -Winline'
 end
 
 create_makefile("typhoeus/native")
